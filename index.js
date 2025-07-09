da82869cedb9cf75b8884fdc1402d39350da7fa5
UmangPansuriya30 <umangpansuriya@weybee.in>
2025-07-09 15:53:02 +0530

    UX Updated

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const slider = new A11YSlider(document.querySelector(".slider"), {
-    adaptiveHeight: false,
-    dots: true,
-    centerMode: true,
-    arrows: false,
-    responsive: {
-        480: {
-            dots: false
-        },
-    },
+const primaryHeader = document.querySelector('.primary-header');
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+navToggle.addEventListener('click', () => {
+    primaryNav.hasAttribute('data-visible')
+        ? navToggle.setAttribute('aria-expanded', false)
+        : navToggle.setAttribute('aria-expanded', true);
+    primaryNav.hasAttribute('data-visible')
+        ? document.documentElement.style.overflowY = 'auto'
+        : document.documentElement.style.overflowY = 'hidden'
+    primaryNav.toggleAttribute('data-visible');
+    primaryHeader.toggleAttribute("data-overlay");
 });
+console.log("sdbfkjbsd");
